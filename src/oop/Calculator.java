9f10fcebf15055c79bc1f99a82e4c3d3cb38b4cf
CGM <mrmouse.guru@gmail.com>
2024-05-22 10:34:42 +0700

    added oop calculating a * b

---
+++ src/oop/Calculator.java
@@ -0,0 +1,19 @@
+package oop;
+
+public class Calculator {
+
+    //fields, attribute, instance
+    int a;
+    int b;
+
+    //methods, function
+    Calculator(){
+        a = 2;
+        b = 2;
+    }
+
+    int multiply(){
+        return a * b;
+    }
+
+}
