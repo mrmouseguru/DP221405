4aa1960885ea940bcd7efdcf3979cf3a806f9463
CGM <mrmouse.guru@gmail.com>
2024-06-05 10:00:13 +0700

    updated full GUI

--- src/buoi3/swing/CalculatorWindow.java
+++ src/buoi3/swing/CalculatorWindow.java
@@ -2,31 +2,52 @@ package buoi3.swing;
 
 import java.awt.Color;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class CalculatorWindow extends JFrame{
 
     //field
     private JPanel jPanelRemote;
-    private JLabel jLabelInputRemote1;
+    private JLabel jLabelInputRemote1, 
+    jLabelInputRemote2, jLabelOutputRemote;
+    private JTextField jTextFieldInputRemote1,
+    jTextFieldInputRemote2;
+    private JButton addButon, suButton, mulButton, divButton;
 
     CalculatorWindow(){
+        buildPanel();
 
         setSize(400, 400);
         setVisible(true);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        add(jPanelRemote);
+
+    }
+
+    public void buildPanel() {
+
         jPanelRemote = new JPanel();
         //jPanelRemote.setBackground(Color.BLUE);
 
         jLabelInputRemote1 = new JLabel("input1");
         jPanelRemote.add(jLabelInputRemote1);
-
-        add(jPanelRemote);
-
+        jTextFieldInputRemote1 = new JTextField(10);
+        jPanelRemote.add(jTextFieldInputRemote1);
+        jLabelInputRemote2 = new JLabel("input2");
+        jPanelRemote.add(jLabelInputRemote2);
+        jTextFieldInputRemote2 = new JTextField(10);
+        jPanelRemote.add(jTextFieldInputRemote2);
+        jLabelOutputRemote = new JLabel("output");
+        jPanelRemote.add(jLabelOutputRemote);
+        addButon = new JButton("ADD");
+        jPanelRemote.add(addButon);
+        
     }
 
 }
