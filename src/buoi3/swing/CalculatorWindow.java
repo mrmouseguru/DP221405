b6d3f54f168ce1138473b6f13c117e01ba367e8d
CGM <mrmouse.guru@gmail.com>
2024-06-05 09:16:31 +0700

    added GUI using Swing Java

---
+++ src/buoi3/swing/CalculatorWindow.java
@@ -0,0 +1,16 @@
+package buoi3.swing;
+
+import javax.swing.JFrame;
+
+public class CalculatorWindow extends JFrame{
+
+    CalculatorWindow(){
+
+        setSize(400, 400);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+}
---
+++ src/buoi3/swing/CalculatorWindowApp.java
@@ -0,0 +1,11 @@
+package buoi3.swing;
+
+public class CalculatorWindowApp {
+    public static void main(String[] args) {
+
+        CalculatorWindow calculatorWindowRemote = 
+        new CalculatorWindow();
+        
+    }
+
+}
