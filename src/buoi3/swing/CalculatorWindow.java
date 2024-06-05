a61a7050f67e127ccc8e95aa443f9829bd74f562
CGM <mrmouse.guru@gmail.com>
2024-06-05 09:27:04 +0700

    added label to panel

--- src/buoi3/swing/CalculatorWindow.java
+++ src/buoi3/swing/CalculatorWindow.java
@@ -1,8 +1,16 @@
 package buoi3.swing;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class CalculatorWindow extends JFrame{
+
+    //field
+    private JPanel jPanelRemote;
+    private JLabel jLabelInputRemote1;
 
     CalculatorWindow(){
 
@@ -10,6 +18,14 @@ public class CalculatorWindow extends JFrame{
         setVisible(true);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        jPanelRemote = new JPanel();
+        //jPanelRemote.setBackground(Color.BLUE);
+
+        jLabelInputRemote1 = new JLabel("input1");
+        jPanelRemote.add(jLabelInputRemote1);
+
+        add(jPanelRemote);
 
     }
 
